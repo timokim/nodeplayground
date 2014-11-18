311136c54726b643e1710d684a97ff85a5721534
unknown <timokim@timothykim.canlab.ibm.com>
2014-11-18 12:08:31 -0500

    gitignore + package.json

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'jade');
 app.set('port', process.env.PORT || 3000);
 
 // uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
+//app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
